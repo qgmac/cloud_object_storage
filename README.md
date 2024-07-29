7614aa7ad2dc911abebe3ebbea32592b043ce8f0
zhanxianchao <qgmac@qq.com>
2024-07-29 16:11:10 +0800

    1.修改使用文档2.完善oss客户端

--- README.md
+++ README.md
@@ -10,9 +10,53 @@ $ composer require qgmac/cloud_object_storage -vvv
 ```
 
 ## Usage
+```php
+$config = [
+        'default' => 'cos',//默认
+        'disks' => [
+            'cos' => [// 腾讯云
+                'driver' => 'cos',
+                'secret_id' => '',
+                'secret_key' => '',
+                'region' => '',
+                'bucket' => '',
+                'url' => '',
+            ],
+            'obs' => [// 华为云
+                'driver' => 'obs',
+                'key' => '',
+                'secret' => '',
+                'bucket' => '',
+                'url' => '',
+                'endpoint' => '',
+            ],
+            'oss' => [// 阿里云
+                'driver' => 'obs',
+                'key' => '',
+                'secret' => '',
+                'role_arn' => '',//即需要扮演的角色ID，格式为acs:ram::$accountID:role/$roleName
+                'bucket' => '',
+                'url' => '',
+                'endpoint' => '',
+            ],
+        ],
+    ];
 
-使用方法查看 tests 目录下的测试用例
 
+$store_key = "/test/test.jpg";
+$manager = new Manager($config);
+$default_store_url = $manager->store()->uploadFile($_file, $head_url);
+
+$cos_url = $manager->store('cos')->uploadFile($_file, $head_url);
+$obs_url = $manager->store('obs')->uploadFile($_file, $head_url);
+
+$oss_url = $manager->store('oss')->uploadFile($_file, $head_url);
+
+$manager->store('cos')->deleteFile($store_key) bool;
+$manager->store('cos')->getFileUrl($store_key) string;
+$manager->store('cos')->exist($store_key) bool;
+```
+ps [OSS配置参考](https://help.aliyun.com/zh/oss/developer-reference/oss-php-configure-access-credentials?spm=a2c4g.11186623.0.0.1adb5d0fKxW76R#9a510df0f0i8d):使用长期访问凭证
 ## Contributing
 
 You can contribute in one of three ways:
--- src/cloud/Oss.php
+++ src/cloud/Oss.php
@@ -8,39 +8,88 @@
 namespace Qgmac\CloudObjectStorage\cloud;
 
 
+use AlibabaCloudCredentialsWrapper;
+use OSS\Core\OssException;
+use OSS\Credentials\StaticCredentialsProvider;
+use AlibabaCloud\Credentials\Credential;
+use OSS\Credentials\CredentialsProvider;
+use OSS\Http\RequestCore_Exception;
+use OSS\OssClient;
+use ReflectionException;
 
 class Oss implements CloudInterface
 {
     private array $cfg;
     private string $bucket;
+    private string $endpoint;
 
+    private $ossClient;
 
+    /**
+     * @throws ReflectionException
+     */
     public function __construct($cfg)
     {
         $this->cfg = $cfg;
         $this->bucket = $this->cfg['bucket'];
+        $this->endpoint = $this->cfg['endpoint'];
+
+        $ramRoleArn = new Credential(array(
+            // 填写Credential类型，固定值为ram_role_arn。
+            'type' => 'ram_role_arn',
+            // 填写RAM用户的访问密钥（AccessKey ID和AccessKey Secret）。
+            'access_key_id' => $this->cfg['key'],
+            'access_key_secret' => $this->cfg['secret'],
+            // 填写RAM角色的RamRoleArn。即需要扮演的角色ID，格式为acs:ram::$accountID:role/$roleName。
+            'role_arn' => 'OSS_STS_ROLE_ARN',
+            // 自定义角色会话名称，用于区分不同的令牌。
+            'role_session_name' => 'oss_role_session_' . mt_rand(1000, 9999) . time(),
+            // 自定义权限策略。
+            'policy' => '',
+        ));
+
+        $provider = (new AlibabaCloudCredentialsWrapper($ramRoleArn))->getCredentials();
+
+        $config = array(
+            'provider' => $provider,
+            'endpoint' => $this->endpoint
+        );
+
+        $this->ossClient = new OssClient($config);
     }
+
     public function uploadFile(string $filePath, string $key): string
     {
-        // TODO: Implement uploadFile() method.
+        try {
+            $this->ossClient->uploadFile($this->bucket, $key, $filePath);
+        } catch (OssException|RequestCore_Exception $e) {
+            return '';
+        }
         return $this->getFileUrl($key);
     }
 
     public function deleteFile(string $key): bool
     {
-        // TODO: Implement deleteFile() method.
+        try {
+            $this->ossClient->deleteObject($this->bucket, $key);
+        } catch (OssException|RequestCore_Exception $e) {
+            return false;
+        }
         return true;
     }
 
     public function getFileUrl(string $key): string
     {
-        // TODO: Implement getFileUrl() method.
         return $this->cfg['url'] . '/' . $key;
     }
 
     public function exist(string $key): bool
     {
-        // TODO: Implement exist() method.
+        try {
+            $this->ossClient->getObject($this->bucket, $key);
+        } catch (OssException|RequestCore_Exception $e) {
+            return false;
+        }
         return true;
     }
 }
